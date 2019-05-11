bcb96d6e8fab82d7f5ca8ec3c10ebfe2ae22bf7d
saburabrina <sabrinascosme@gmail.com>
2019-05-11 16:51:23 -0300

    Início da tela de montar a grade pt2

---
+++ codigo_fonte/view/script/scriptGrade2.js
@@ -0,0 +1,7 @@
+function expandir_botao(id, obj){
+	botao = document.getElementById(id);
+	obj.style.backgroundColor = "#68BBF1";
+	obj.style.color = "#2b4654";
+	obj.style.fontWeight = "bold";
+
+}
